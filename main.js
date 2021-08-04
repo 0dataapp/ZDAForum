f943a0c799ca1fb1e37fda9087514211301c70cb
Rosano Coutinho <pub@xeari.com>
2021-08-04 16:20:19 -0300

    Fix typo

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const mod = {
 
 	ZDAForumHotfixPatches () {
 		return {
-			'../node_modules/ROCOForum/main.ejs': {
+			'./node_modules/ROCOForum/main.ejs': {
 				'?category=<%= ROCOForumTopic %>&amp;': '?alfa=bravo&amp;',
 				'" category="<%= ROCOForumTopic %>" per-page': '"  per-page',
 				'/c/<%= ROCOForumTopic %>': '/',
