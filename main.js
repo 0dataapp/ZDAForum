fc5dd99979a2cf8ced7bfc2f4aeca7d224f4b096
Rosano Coutinho <pub@xeari.com>
2021-08-04 16:03:03 -0300

    Scaffold project

---
+++ main.js
@@ -0,0 +1,15 @@
+const mod = {
+
+	ZDAForumHotfixPatches () {
+		return {
+			'../node_modules/ROCOForum/main.ejs': {
+				'?category=<%= ROCOForumTopic %>&amp;': '?alfa=bravo&amp;',
+				'" category="<%= ROCOForumTopic %>" per-page': '"  per-page',
+				'/c/<%= ROCOForumTopic %>': '/',
+			},
+		};
+	},
+
+};
+
+Object.assign(exports, mod);
